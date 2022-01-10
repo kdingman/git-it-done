913608f85e8b498552836bf6836ca7871b244225
Kellly <kcdonlan@yahoo.com>
2022-01-10 16:13:34 -0500

    api url

---
+++ assets/js/homepage.js
@@ -0,0 +1,14 @@
+var getUsersRepos = function(user) {
+    // format the github api url
+    var apiUrl = "https://api.github.com/users/" + user + "/repos";
+    
+    // make a request to the url
+    fetch(apiUrl).then(function(response){
+        response.json().then(function(data){
+            console.log(data);
+        });
+    });
+}
+
+
+getUsersRepos();
