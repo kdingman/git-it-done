5d2d99b1ec5b1e56b2ff5e5c225ea9207ee8c7f0
Kellly <kcdonlan@yahoo.com>
2022-01-11 16:20:19 -0500

    reponame and issues

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,5 +1,23 @@
 var issueContainerEl = document.querySelector("#issues-container");
-var liimitWarningEl = document.querySelector("#limit-warning");
+var limitWarningEl = document.querySelector("#limit-warning");
+var repoNameEl = document.querySelector("#repo-name");
+
+var getRepoName = function() {
+    // grab repo name from url query string
+    var queryString = document.location.search;
+    var repoName = queryString.split("=")[1];
+
+    if(repoName) {
+        // display repo name on page
+        repoNameEl.textContent = repoName;
+
+        getRepoIssues(repoName);
+    }
+    else {
+        // if no repo was given, redirect to the homepage
+        document.location.replace("./index.html");
+    }
+};
 
 var getRepoIssues = function(repo) {
     var apiUrl = "https://api.github.com/repos" + repo + "/issues?direction=asc";
@@ -13,15 +31,14 @@ var getRepoIssues = function(repo) {
 
                 // check if api has paginated issues
                 if(response.headers.get("Link")){
-                    console.log("repo has more than 30 issues");
+                    displayWarning(repo);
                 }
             });
         }
         else {
-            alert("There was a problem with your request");
+            document.location.replace("./index.html");
         }
     });
-getRepoIssues("facebook/react");
 
 var displayIssues = function(issues){
     if(issues.length === 0) {
@@ -74,3 +91,5 @@ var displayWarning = function(repo) {
     // append to warning container
     liimitWarningEl.appendChild(linkEl);
 };
+
+getRepoName();
