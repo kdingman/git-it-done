8ba3c2e9f206c5072747016ffc79f747957d976b
Kellly <kcdonlan@yahoo.com>
2022-01-11 15:48:16 -0500

    extra message

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,4 +1,5 @@
 var issueContainerEl = document.querySelector("#issues-container");
+var liimitWarningEl = document.querySelector("#limit-warning");
 
 var getRepoIssues = function(repo) {
     var apiUrl = "https://api.github.com/repos" + repo + "/issues?direction=asc";
@@ -9,7 +10,11 @@ var getRepoIssues = function(repo) {
             response.json().then(function(data) {
                 // pass response data to dom function
                 displayIssues(data);
-                console.log(data);
+
+                // check if api has paginated issues
+                if(response.headers.get("Link")){
+                    console.log("repo has more than 30 issues");
+                }
             });
         }
         else {
@@ -55,4 +60,17 @@ var displayIssues = function(issues){
     // append to the dom
     issueContainerEl.appendChild(issueEl);
     }
+};
+
+var displayWarning = function(repo) {
+    // add text to warning container
+    liimitWarningEl.textContent = "To see more than 30 issues, visit ";
+
+    var linkEl = document.createElement("a");
+    linkEl.textContent = "See More Issues on GitHub.com";
+    linkEl.setAttribute("href", "https://github.com/" + repo + "/issues");
+    linkEl.setAttribute("target", "_blank");
+
+    // append to warning container
+    liimitWarningEl.appendChild(linkEl);
 };
